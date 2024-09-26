4fe8f6c049c2b9a45052672b0ab2e8744bb9c4ba
rahma <fijriatirahmaturrizqi@gmail.com>
2024-09-26 07:06:29 +0700

    feat: percobaan 1 praktikum 5

---
+++ PemilihanBilangan10.java
@@ -0,0 +1,14 @@
+package praktikum05;
+import java.util.Scanner;
+public class PemilihanBilangan10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+        if(angka%2 == 0){
+            System.out.println("Angka " + angka + " termasuk bilangan genap");
+        }else{
+            System.out.println("Angka "+angka+ " termasuk bilangan ganjil");
+        }
+    }    
+}
