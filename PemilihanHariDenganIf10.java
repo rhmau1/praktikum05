fc925098bf8268db8ca42834ec66ed31b1a4cbc2
rahma <fijriatirahmaturrizqi@gmail.com>
2024-09-26 07:22:01 +0700

    feat: pemilihan hari dengan if bilangan bulat

---
+++ PemilihanHariDenganIf10.java
@@ -0,0 +1,16 @@
+package praktikum05;
+import java.util.Scanner;
+public class PemilihanHariDenganIf10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka antara (1-7): ");
+        int input = sc.nextInt();
+        if(input == 1 || input == 2 || input == 3 || input == 4 || input == 5){
+            System.out.println("Weekday");
+        }else if(input == 6 || input == 7){
+            System.out.println("Weekend");
+        }else{
+            System.out.println("invalid number");
+        }
+    }
+}
