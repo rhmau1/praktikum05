f8763989c9d3ba1c521629a555475bfc3b1b8ac8
rahma <fijriatirahmaturrizqi@gmail.com>
2024-09-26 07:11:44 +0700

    update: modifikasi struktur pemilihan menggunakan ternary operator

--- PemilihanBilangan10.java
+++ PemilihanBilangan10.java
@@ -5,10 +5,12 @@ public class PemilihanBilangan10 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
-        if(angka%2 == 0){
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }else{
-            System.out.println("Angka "+angka+ " termasuk bilangan ganjil");
-        }
+        // if(angka%2 == 0){
+        //     System.out.println("Angka " + angka + " termasuk bilangan genap");
+        // }else{
+        //     System.out.println("Angka "+angka+ " termasuk bilangan ganjil");
+        // }
+        String hasil = (angka%2 == 0) ? "Angka " + angka + " termasuk bilangan genap" : "Angka " + angka + " termasuk bilangan ganjil";
+        System.out.println(hasil);
     }    
 }
